2d8e7d76ca83649cf22f512a9d529b3426f831c4
daffaulhaq <udaffa09@gmail.com>
2021-12-05 22:07:13 +0700

    first commit

---
+++ P5_1_1204045/P5_1_1204045_Host/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace P5_1_1204045_Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.verticalLabel1 = new P5_1_1204045.VerticalLabel();
+            this.numericTextBox1 = new P5_1_1204045.NumericTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Extended Control : Numeric TextBox";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Custom Control : Vertical Label";
+            // 
+            // verticalLabel1
+            // 
+            this.verticalLabel1.Location = new System.Drawing.Point(384, 224);
+            this.verticalLabel1.Name = "verticalLabel1";
+            this.verticalLabel1.Size = new System.Drawing.Size(26, 77);
+            this.verticalLabel1.TabIndex = 7;
+            this.verticalLabel1.Text = "VerticalLabel";
+            // 
+            // numericTextBox1
+            // 
+            this.numericTextBox1.Location = new System.Drawing.Point(213, 151);
+            this.numericTextBox1.Name = "numericTextBox1";
+            this.numericTextBox1.Size = new System.Drawing.Size(374, 20);
+            this.numericTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.verticalLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private P5_1_1204045.NumericTextBox numericTextBox1;
+        private System.Windows.Forms.Label label2;
+        private P5_1_1204045.VerticalLabel verticalLabel1;
+    }
+}
+
